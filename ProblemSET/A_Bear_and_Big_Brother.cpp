38a059e8465dd69c852b42b018b7788e87495b25
sheikhshakibhossain <sheikhshakibpro@gmail.com>
2023-03-04 01:36:54 +0600

    update

---
+++ ProblemSET/A_Bear_and_Big_Brother.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main() {
+    
+    int a,b;
+    cin >> a;
+    cin >> b;
+    int count = 0;
+    while ( a<=b ) {
+        count++;
+        a*=3;
+        b*=2;
+    }
+    cout << count << endl;
+    return 0;
+}
